8d4e43af0d1fbc66561a159a2fb7ed8213460023
gordongogah <gordongogah9@gmail.com>
2022-07-03 10:48:10 +0300

    commit tree c523adceec4d05626ec7eaaa5be8d4d2f1af32f7

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (distance <= 400){
+    return 'This one is on me!';
+  }else if(distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.'
+  }
 }
 
 function ternaryCheckCity(){
